6acec89af7cee3acc67d89b5ead40afc40938c96
SergioRuyDev <sergioruyenator@gmail.com>
2020-11-15 15:14:35 +0400

    Model done, exception runtime started.

---
+++ src/main/java/com/ruydev/redditcloneapi/exceptions/SpringRedditException.java
@@ -0,0 +1,11 @@
+package com.ruydev.redditcloneapi.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
